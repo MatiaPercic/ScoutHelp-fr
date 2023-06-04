fddd065b59a1b071972eeac6a51cee9910ba0c4c
MatiaPercic <mapercic@unipu.hr>
2023-06-04 12:54:21 +0200

    opis sto je scouthelp

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 title="title">Što je ScoutHelp?</h1>
+
+    <br>
+
+    <p class="plain">
+      ScoutHelp je webaplikacija s ulogom evidentiranja i pregleda izvršenog
+      volontiranja unutar izviđačke jedinice
+   
+    <br>
+     <br>
+     <br>
+      Ideja je nastala iz potrebe za kozistentim i efikasnim načinom bilježenja izvršenih volonterskih sati,
+      zajedno s mogućnošću pregleda detalja vezanih za pojedinu volontersku aktivnost.
+    
+    <br>
+    <br>
+    <br>
+
+      Fokus na valontiranje isključivo unutar izviđačke jedinice odabran je zbog poznavanja strukture izviđača, 
+      mogućih oblika rada te vrsta aktivnosti, koji su svi neophodni za potpuni i točan prikaz izvršenog volontiranja.
+
+      
+    <br>
+    <br>
+
+  </p>
   </div>
 </template>
+
+<style scoped>
+
+.plain {
+
+  
+  font-size: 20px;
+  font-weight: bold;
+  margin: 30px;
+ 
+}
+
+</style>
