dbd7a9d7caf9d60a86c6b891e1de3b1873f07771
MatiaPercic <mapercic@unipu.hr>
2023-06-10 14:54:52 +0200

    profilVolonter

---
+++ src/views/ProfilVolonter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <h1>Profilna stranica za volontere</h1>
+
+<!--     <div class="container">
+      <div class="row">
+        <div class="col">Ime: </div>   
+        <div class="col-md-auto">{{ volonter.ime }}</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Prezime: </div>   
+        <div class="col-md-auto">{{ volonter.prezime }}</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Godine: </div>   
+        <div class="col-md-auto">{{ volonter.godine }}</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Broj odrađenih aktivnosti: </div>   
+        <div class="col-md-auto">{{ volonter.broj_aktivnosti }}</div>
+      </div>
+      
+      <div class="row">
+        <div class="col">Broj volonterskih sati: </div>   
+        <div class="col-md-auto">{{ volonter.broj_sati }}</div>
+      </div>
+
+    </div> -->
+  </div>
+</template>
+
+
+<script>
+import { onMounted } from 'vue';
+
+
+
+
+</script>
