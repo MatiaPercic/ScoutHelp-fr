f92f60cd0548a48d16486d0218a85382f461bfe3
MatiaPercic <mapercic@unipu.hr>
2023-06-17 20:38:30 +0200

    uspiješno implementiran pagination

--- src/views/Aktivnosti.vue
+++ src/views/Aktivnosti.vue
@@ -3,7 +3,7 @@
       
       <h1 class="title">Pregled aktivnosti </h1>
       
-      <table>
+      <table class="table table-striped">
         <thead>
             <tr>
                 <th>Datum</th>
@@ -16,7 +16,7 @@
         </thead>
 
         <tbody>
-            <tr v-for="akt in aktivnosti" :key="akt._id">
+            <tr  v-for="akt in records" :key="akt._id" v-if="akt">
                 <td>{{ akt.datum }}</td>
                 <td>{{ akt.opis }}</td>
                 <td>{{ formatArray( akt.oblik_rada)}}</td>
@@ -53,29 +53,43 @@ export default{
             totalItems:0,
             aktivnosti:[],
             perPage:3,
-            currentPage:1
+            currentPage:1,
+            records:[]
 
         };
     },
     created(){
-        this.findAktivnosti();
+        this.findAktivnosti()        
     },
+    computed: {
+  totalPages() {
+    return Math.ceil(this.aktivnosti.length / this.perPage);
+  }
+},
+
     methods:{
-       async findAktivnosti(page=1){
-           const response= await axios.post("http://localhost:3001/aktivnostiAll",{
-            
-            page: this.currentPage,
-            limit: this.perPage,
-          });
-        this.aktivnosti=response.data;
-        this.totalItems=response.data.length;
-        console.log(this.perPage)
-        console.log(this.aktivnosti.length)
+       findAktivnosti(){
+            axios
+            .post("http://localhost:3001/aktivnostiAll")
+            .then((response)=>{
+                this.aktivnosti=response.data;
+                this.totalItems=response.data.length;
+                this.promjeneStr(this.currentPage);
+                console.log(this.totalItems);
+                console.log(this.currentPage);
+            })
     },
 
-    promjeneStr(page) {
-      this.currentPage = page;
-      this.findAktivnosti();
+    promjeneStr: function(currentPage) {
+      console.log(this.currentPage);
+      this.records=[];
+      let start=this.perPage*(this.currentPage-1);
+        let end=start+this.perPage-1;
+        for(let i=start;i<=end;i++){
+            if(this.aktivnosti[i])
+                this.records[i]=this.aktivnosti[i];
+        };
+        console.log(this.records);
     },
     formatArray(array) {
       return array.length > 0 ? array.join(', ') : '-';
