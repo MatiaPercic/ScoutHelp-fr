573d0415a051ad9af7e7d1779b12b8e3459e7c46
MatiaPercic <mapercic@unipu.hr>
2023-06-21 19:31:36 +0200

    potpun izgled unosa aktivnosti

--- src/views/NovaAktivnost.vue
+++ src/views/NovaAktivnost.vue
@@ -2,64 +2,134 @@
   <div class="about">
     <h1>Unos nove aktivnosti</h1>
 
-    <div class="container">
+    <div class="grupni">
+      <div class="row row1">
+        <div class="col-4 custom-date">
+          <label for="example-datepicker" class="cal-label"
+            >Odaberi datum</label
+          >
+          <b-form-datepicker
+            id="example-datepicker"
+            placeholder="Odaberi datum"
+            v-model="value"
+            class="mb-2"
+            :start-weekday="1"
+            :date-format-options="{
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+            }"
+            menu-class="w-100"
+            calendar-width="100%"
+            locale="hr"
+          ></b-form-datepicker>
 
-      <div class="row">
-      <div class="col-4">
+          <p>Prikaz u tablici aktivnosti: '{{ value }}'</p>
+        </div>
 
-      
-        <label for="example-datepicker" class="cal-label">Odaberi datum</label>
-      <b-form-datepicker 
-    id="example-datepicker" 
-    placeholder="Odaberi datum"
-     v-model="value" class="mb-2" 
-     :start-weekday=1
-     :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-     menu-class="w-100"
-     calendar-width="100%"
-     locale="hr"></b-form-datepicker>
+        <div class="col-md-5 custom-opis" style="align-self: center">
+          <input
+            class="opis-custom"
+            v-model="odabirOpis"
+            type="text"
+            id="input-opis"
+            placeholder="Unesi Opis"
+          />
+        </div>
 
-    <p>Prikaz u tablici aktivnosti: '{{ value }}'</p>
+        <div class="col-md-2 custom-sati">
+          <label for="satiID"><b>Unesi broj sati</b></label>
+          <input type="number" id="satiID" v-model="odabirSati" min="1" />
+        </div>
+      </div>
 
-  </div>
-
-  <div class="col-md-7 ">
-    <input
-    class="opis-custom"
-    type="text"
-    id="input-opis"
-    placeholder="Unesi Opis"/>
-  </div>
-
-</div>
-
-<div class="row">
-
-  <div class="col-md-2">
-    <p>Trenutan Odabir:</p>
-    <p v-for="volonter in new_aktivnost.volonteri" :key="volonter"> {{ volonter }}</p>
-  </div>
-  <div class="col-md-4">
-    <b-form-select v-model="odabirVolonter" @input="dodajVolonter" class="custom-volInput" >
-      <option value="" disabled selected>Odaberi volontere</option>
-            <option v-for="volonter in volonteriOpcije" :key="volonter" :value="volonter">
+      <div class="row row2">
+        <div class="col-md-4">
+          <b-form-select
+            v-model="odabirVolonter"
+            @input="dodajVolonter"
+            class="custom-volInput"
+          >
+            <option value="" disabled selected>Odaberi volontere</option>
+            <option
+              v-for="volonter in volonteriOpcije"
+              :key="volonter"
+              :value="volonter"
+            >
               {{ volonter }}
             </option>
           </b-form-select>
 
-          <button @click="volIzbrisi">Izbriši odabir</button>
-          
-  </div>
+          <button @click="volIzbrisi" class="cancelVol">Izbriši odabir</button>
 
-</div>
+          <b-form-select
+            v-model="odabirAdmini"
+            @input="dodajAdmin"
+            class="custom-admInput"
+          >
+            <option value="" disabled selected>Odaberi administratore</option>
+            <option v-for="adm in adminiOpcije" :key="adm" :value="adm">
+              {{ adm }}
+            </option>
+          </b-form-select>
+
+          <button class="cancelAdm" @click="admIzbrisi">Izbriši odabir</button>
+
+          <b-form-select
+            v-model="odabirOblici"
+            @input="dodajOblik"
+            class="custom-oblInput"
+          >
+            <option value="" disabled selected>Odaberi oblike rada</option>
+            <option v-for="oblik in obliciOpcije" :key="oblik" :value="oblik">
+              {{ oblik }}
+            </option>
+          </b-form-select>
+
+          <button @click="oblIzbrisi" class="cancelObl">Izbriši odabir</button>
+        </div>
+
+        <div class="col-md-2 custom-volIspis">
+          <p>Trenutan Odabir Volontera:</p>
+          <b v-for="volonter in new_aktivnost.volonteri" :key="volonter">
+            {{ volonter }}
+            <br />
+          </b>
+        </div>
+
+        <!--  -->
+        <div class="col-md-3 custom-admIspis">
+          <p>Trenutan Odabir administratora:</p>
+          <b v-for="adm in new_aktivnost.admin" :key="adm">
+            {{ adm }}
+            <br />
+          </b>
+        </div>
+
+        <div class="col-md-2 custom-oblIspis">
+          <p>Trenutan Odabir oblika rada:</p>
+          <b v-for="oblik in new_aktivnost.oblik_rada" :key="oblik">
+            {{ oblik }}
+            <br />
+          </b>
+        </div>
+      </div>
+
+      <div class="row row3 justify-content-center">
+        <div class="col-2">
+          <button class="profButton">Povratak na profil</button>
+        </div>
+        <div class="col-2">
+        <button class="newaktButton">Unesi novu aktivnost</button>
+      </div>
+      </div>
 
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
+import axios from "axios";
 
 export default {
   name: "novaAktivnost",
@@ -69,46 +139,94 @@ export default {
       value: null,
       weekday: 1,
       hideHeader: true,
-      odabirVolonter:"",
-      volonteriOpcije:[],
 
-      new_aktivnost:{
-        datum:"",
-        opis:"",
-        oblik_rada:[],
-        volonteri:[],
-        admin:[],
-        sati:""
+      odabirVolonter: "",
+      volonteriOpcije: [],
+
+      adminiOpcije: [],
+      odabirAdmini: "",
+
+      odabirOblici: "",
+      obliciOpcije: [],
+
+      odabirSati: 0,
+      odabirOpis: "",
+
+      new_aktivnost: {
+        datum: "",
+        opis: "",
+        oblik_rada: [],
+        volonteri: [],
+        admin: [],
+        sati: "",
       },
     };
   },
-  created(){
-    this.getVolonteriOpcije()
+  created() {
+    this.getVolonteriOpcije();
+    this.getAdminiOpcije();
+    this.getObliciOpcije();
   },
 
   methods: {
-    getVolonteriOpcije(){
-        axios
-        .get("http://localhost:3001/sviVolonteri")
-        .then((response)=>{
-            for(let i=0;i<response.data.length;i++)
-              this.volonteriOpcije.push(response.data[i].email);
-
-              console.log(this.volonteriOpcije);
-        });
-
+    getVolonteriOpcije() {
+      axios.get("http://localhost:3001/volonteriEmail").then((response) => {
+        for (let i = 0; i < response.data.length; i++)
+          this.volonteriOpcije.push(response.data[i].email);
+      });
     },
-    dodajVolonter(){
-      if(this.odabirVolonter){
+    dodajVolonter() {
+      if (
+        this.odabirVolonter &&
+        !this.new_aktivnost.volonteri.includes(this.odabirVolonter)
+      ) {
         this.new_aktivnost.volonteri.push(this.odabirVolonter);
-        this.odabirVolonter="";
+        this.odabirVolonter = "";
       }
 
       console.log(this.new_aktivnost.volonteri);
     },
-    volIzbrisi(){
-      this.new_aktivnost.volonteri=[];
-    }
+    volIzbrisi() {
+      this.new_aktivnost.volonteri = [];
+    },
+
+    getAdminiOpcije() {
+      axios.get("http://localhost:3001/adminiEmail").then((response) => {
+        for (let i = 0; i < response.data.length; i++)
+          this.adminiOpcije.push(response.data[i].email);
+      });
+    },
+    dodajAdmin() {
+      if (
+        this.odabirAdmini &&
+        !this.new_aktivnost.admin.includes(this.odabirAdmini)
+      ) {
+        this.new_aktivnost.admin.push(this.odabirAdmini);
+        this.odabirAdmini = "";
+      }
+    },
+    admIzbrisi() {
+      this.new_aktivnost.admin = [];
+    },
+
+    getObliciOpcije() {
+      axios.get("http://localhost:3001/obliciRada").then((response) => {
+        for (let i = 0; i < response.data.length; i++)
+          this.obliciOpcije.push(response.data[i].opis);
+      });
+    },
+    dodajOblik() {
+      if (
+        this.odabirOblici &&
+        !this.new_aktivnost.oblik_rada.includes(this.odabirOblici)
+      ) {
+        this.new_aktivnost.oblik_rada.push(this.odabirOblici);
+        this.odabirOblici = "";
+      }
+    },
+    oblIzbrisi() {
+      this.new_aktivnost.oblik_rada = [];
+    },
   },
 };
 </script>
@@ -118,22 +236,21 @@ h1 {
   margin-top: 30px;
 }
 
-.container{
+.grupni {
   margin-left: 20px;
   margin-right: 20px;
 }
-.row{
-  margin-bottom: 50px;
+.row1 .row2 {
+  margin-top: 50px;
 }
-.cal-label{
+.cal-label {
   margin-left: 0%;
   text-align: left;
   font-size: 20px;
 }
 
-.opis-custom{
-  margin-top: 35px;
-  width: 600px;
+.opis-custom {
+  width: 400px;
   text-align: justify;
   font-size: 18px;
   opacity: 65%;
@@ -141,11 +258,31 @@ h1 {
   border-width: 2px;
 }
 
-.custom-volInput{
-  width: 300px;
+select {
+  width: 250px;
   height: 30px;
   font-size: 15px;
   margin-left: -30px;
+  margin-bottom: 50px;
+}
+
+.custom-sati {
+  margin-top: 30px;
+}
+
+.custom-date {
+  margin-top: 20px;
+}
+
+button {
+  margin: 5px;
+}
+
+.row3{
+  margin-top: 20px;
+}
+.newaktButton .profButton{
+  width: 100px;
 
 }
 </style>
